02cdf66bda539b24c7aac6014c23d375c6382c89
Shravan Motipally <numtheory@ymail.com>
2019-08-03 12:43:59 -0500

    @smotipally Experimenting...

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@ fn main() {
 	println!("Guess the number!");
 	println!("Please input your guess.");
 	let mut guess = String::new();
+	let failure_msg = String::new();
 
 	io::stdin().read_line(&mut guess)
-		.expect("Failed to read line");
+		.expect(failure_msg);
 
-	println!("You guessed: {}", guess);
+    println!("You guessed: {}", guess);
+
+	println!("Guess another number!");
+	io::stdin().read_line(&mut guess)
+		.expect(failure_msg);
 }
